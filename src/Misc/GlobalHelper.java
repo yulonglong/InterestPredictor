cf09e9fe8504c05934ab0b6eb7543061e4d4e5f4
jials <jiale@comp.nus.edu.sg>
2016-03-16 00:10:03 +0800

    Add new lib file and GlobalHelper

---
+++ src/Misc/GlobalHelper.java
@@ -0,0 +1,11 @@
+package Misc;
+public class GlobalHelper {
+	public static final String pathToTestFacebook = "./dataset/Test/Facebook";
+	public static final String pathToTestLinkedIn = "./dataset/Test/LinkedIn";
+	public static final String pathToTestTwitter = "./dataset/Test/Twitter";
+	public static final String pathToTestGroundTruth = "./dataset/Test/GroundTruth";
+	public static final String pathToTrainFacebook = "./dataset/Train/Facebook";
+	public static final String pathToTrainLinkedIn = "./dataset/Train/LinkedIn";
+	public static final String pathToTrainTwitter = "./dataset/Train/Twitter";
+	public static final String pathToTrainGroundTruth = "./dataset/Train/GroundTruth";
+}
